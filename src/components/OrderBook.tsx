08b3e3ee6af48bc7279e1a5955ccac36f750b648
Nick <escoffier.n@gmail.com>
2021-10-13 21:36:23 -0700

    Add Spread + Title + update styles

--- src/components/OrderBook.tsx
+++ src/components/OrderBook.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 
 import {
-  computeOrdersTotal
+  computeOrdersTotal,
+  formatNumberForDisplay
 } from '../utils/functions';
 
 import OrdersTable from './OrdersTable';
@@ -12,6 +13,8 @@ function OrderBook() {
   const [ws, setWs]: [any, Function] = useState(null);
   const [product, setProduct]: [string, Function] = useState('');
   const [data, setData]: [{asks: TotalOrders, bids: TotalOrders}, Function] = useState({asks: [], bids: []});
+  const spread: number = (data.asks.length && data.bids.length) ? Math.abs(data.asks[0][0] - data.bids[0][0]) : 0;
+  const spreadPercentage: number = spread > 0 ? (spread * 100 / data.asks[0][0]) : 0;
 
   useEffect(() => {
     const newWebsocket = new OrderBookWebSocket({
@@ -46,13 +49,25 @@ function OrderBook() {
     };
   }, [product, ws]);
 
+  const spreadElt = <h2 className="ob-spread">
+    Spread:&nbsp;<span className="number">{ formatNumberForDisplay(spread, 1) }</span>&nbsp;(<span className="number">{ formatNumberForDisplay(spreadPercentage, 2) }</span>%)
+  </h2>;
+
+
   return (
-    <div className="ob">
+    <section className="ob">
+      <div className="ob-title-container">
+        <h1 className="ob-title">
+          Order Book
+        </h1>
+        { spreadElt }
+      </div>
       <div className="ob-tables-container">
         <OrdersTable
           type="ask"
           orders={ data.asks }
         />
+        { spreadElt }
         <OrdersTable
           type="bid"
           orders={ data.bids }
@@ -64,7 +79,7 @@ function OrderBook() {
       } }>
         Switch
       </button>
-    </div>
+    </section>
   );
 }
 
